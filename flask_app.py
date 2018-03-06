ff32f799298eedbdc50e416fdf0f9c4a4082b672
Andrei <andrei08vl@hotmail.com>
2018-03-06 14:25:29 +0000

    Update pytone file

--- flask_app.py
+++ flask_app.py
@@ -13,4 +13,11 @@ The "app.run()" is used to run the app
 The default server address is  "127.0.0.1:5000" 
 '''
 if __name__ == "__main__":
-    app.run()
+    
+'''
+Debug mode has two advantages:
+- If you change any of the files in your project, the app restarts automatically to include those changes
+- If there’s an error, the app displays information about it in your browser
+'''
+
+    app.run(debug=True)
