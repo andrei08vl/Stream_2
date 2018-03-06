b7aa1bc3baa055e34e0b9f4dd023865099eff835
Andrei <andrei08vl@hotmail.com>
2018-03-06 14:27:34 +0000

    Update pytone file with def index():

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
  
 @app.route("/")
-def hello():
-    return "My web app"
+def index():
+    return render_template("index.html")
 
 '''
 The  "if __name__ == ‘__main__’" is used to ensure
